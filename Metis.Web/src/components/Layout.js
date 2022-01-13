88e8e68aa1302e4330d913e44470221c4f5a935e
Ivan Porta <ivanporta@microsoft.com>
2022-01-13 17:29:40 +0100

    Fix sidebar

--- Metis.Web/src/components/Layout.js
+++ Metis.Web/src/components/Layout.js
@@ -21,9 +21,9 @@ export class Layout extends Component {
         return (
             <div className={`g-sidenav-show ${this.state.isNavbarVisible ? 'g-sidenav-pinned' : ''}`}>
                 <Sidebar />
-                <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+                <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
                     <div className="container-fluid my-3">
-                        <div className="row d-block d-sm-none">
+                        <div className="row d-block d-lg-none">
                             <div className="col text-end">
                                 <p className="text-md text-muted pointer" onClick={() => this.onClickToggleNavbar()}>
                                     <FontAwesomeIcon className='opacity-10' icon={faBars} />
